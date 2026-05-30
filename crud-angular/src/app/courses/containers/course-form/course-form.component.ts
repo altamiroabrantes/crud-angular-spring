a04b6071f3c4da411879b6694527147210d7ee23
altamiro.abrantes <altamiro.abrantes@gmail.com>
2026-05-30 17:51:07 -0300

    Componentes Inteligentes x Componentes de Apresentação

---
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, NonNullableFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { CoursesService } from '../../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.scss']
+})
+export class CourseFormComponent implements OnInit {
+
+  form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      category: ['',[Validators.required]]
+    });
+
+  constructor(private formBuilder: NonNullableFormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {
+   // this.form
+
+   }
+
+  ngOnInit(): void { }
+
+  onSubmit(){
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSuccess(), error => this.onError());
+  }
+
+  onCancel(){
+    this.location.back();
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Curso salvo com sucesso.','', { duration: 5000});
+    this.onCancel();
+  }
+  private onError(){
+    this.snackBar.open('Erro ao salvar curso.','', { duration: 5000});
+  }
+}
