eecabf6010639b3e6c598dddfb795c5dcb43fd8d
altamiro.abrantes <altamiro.abrantes@gmail.com>
2026-05-30 19:15:19 -0300

    Atualizar Curso: Salvando o Formulário

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -26,6 +26,17 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>): Observable<Course> {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>): Observable<Course> {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>): Observable<Course> {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
